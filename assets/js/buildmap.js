ad00d07738a713a9ce0a6ae90483b3a88a43109c
Chris Ryan <creditdesign@gmail.com>
2015-02-06 15:02:42 +0000

    Add clip path

--- assets/js/buildmap.js
+++ assets/js/buildmap.js
@@ -34,54 +34,65 @@ function buildmap () {
 					.attr("width", width)
 					.attr("height", height);
 
+	var clipGroupSvg = svg.append("g").attr("clip-path", "url(#clip)");
+
 	svg.call(d3.behavior.zoom()
 			.translate([0, 0])
 			.scale(1)
 			.scaleExtent([1, 8])
 			.on("zoom", zoomed));
 
-	var countriesSVG = svg.append("g");
-	var bordersSVG = svg.append("g");
+	var clip = svg.append("defs").append("svg:clipPath")
+					.attr("id", "clip")
+				  .append("svg:rect")
+					.attr("x", 0)
+					.attr("y", 0)
+					.attr("width", width)
+					.attr("height", height);
+
+	var countriesSVG = clipGroupSvg.append("g");
+	var bordersSVG = clipGroupSvg.append("g");
+
 
 	d3.csv("data/cases-by-who-region-edit.csv", function (d) {
 		/* Convert the text numbers into real numbers */
 		/* If the number is "" make it zero */
 		d.forEach(function (element) {
-			element["1980"] = parseInt(element["1980"], 10) || 0;
-			element["1981"] = parseInt(element["1981"], 10) || 0;
-			element["1982"] = parseInt(element["1982"], 10) || 0;
-			element["1983"] = parseInt(element["1983"], 10) || 0;
-			element["1984"] = parseInt(element["1984"], 10) || 0;
-			element["1985"] = parseInt(element["1985"], 10) || 0;
-			element["1985"] = parseInt(element["1985"], 10) || 0;
-			element["1986"] = parseInt(element["1986"], 10) || 0;
-			element["1987"] = parseInt(element["1987"], 10) || 0;
-			element["1988"] = parseInt(element["1988"], 10) || 0;
-			element["1989"] = parseInt(element["1989"], 10) || 0;
-			element["1990"] = parseInt(element["1990"], 10) || 0;
-			element["1991"] = parseInt(element["1991"], 10) || 0;
-			element["1992"] = parseInt(element["1992"], 10) || 0;
-			element["1993"] = parseInt(element["1993"], 10) || 0;
-			element["1994"] = parseInt(element["1994"], 10) || 0;
-			element["1995"] = parseInt(element["1995"], 10) || 0;
-			element["1996"] = parseInt(element["1996"], 10) || 0;
-			element["1997"] = parseInt(element["1997"], 10) || 0;
-			element["1998"] = parseInt(element["1998"], 10) || 0;
-			element["1999"] = parseInt(element["1999"], 10) || 0;
-			element["2000"] = parseInt(element["2000"], 10) || 0;
-			element["2001"] = parseInt(element["2001"], 10) || 0;
-			element["2002"] = parseInt(element["2002"], 10) || 0;
-			element["2003"] = parseInt(element["2003"], 10) || 0;
-			element["2004"] = parseInt(element["2004"], 10) || 0;
-			element["2005"] = parseInt(element["2005"], 10) || 0;
-			element["2006"] = parseInt(element["2006"], 10) || 0;
-			element["2007"] = parseInt(element["2007"], 10) || 0;
-			element["2008"] = parseInt(element["2008"], 10) || 0;
-			element["2009"] = parseInt(element["2009"], 10) || 0;
-			element["2010"] = parseInt(element["2010"], 10) || 0;
-			element["2011"] = parseInt(element["2011"], 10) || 0;
-			element["2013"] = parseInt(element["2013"], 10) || 0;
-			element["2014"] = parseInt(element["2014"], 10) || 0;
+			element["1980"] = parseInt(element["1980"], 10) || "noData";
+			element["1981"] = parseInt(element["1981"], 10) || "noData";
+			element["1982"] = parseInt(element["1982"], 10) || "noData";
+			element["1983"] = parseInt(element["1983"], 10) || "noData";
+			element["1984"] = parseInt(element["1984"], 10) || "noData";
+			element["1985"] = parseInt(element["1985"], 10) || "noData";
+			element["1985"] = parseInt(element["1985"], 10) || "noData";
+			element["1986"] = parseInt(element["1986"], 10) || "noData";
+			element["1987"] = parseInt(element["1987"], 10) || "noData";
+			element["1988"] = parseInt(element["1988"], 10) || "noData";
+			element["1989"] = parseInt(element["1989"], 10) || "noData";
+			element["1990"] = parseInt(element["1990"], 10) || "noData";
+			element["1991"] = parseInt(element["1991"], 10) || "noData";
+			element["1992"] = parseInt(element["1992"], 10) || "noData";
+			element["1993"] = parseInt(element["1993"], 10) || "noData";
+			element["1994"] = parseInt(element["1994"], 10) || "noData";
+			element["1995"] = parseInt(element["1995"], 10) || "noData";
+			element["1996"] = parseInt(element["1996"], 10) || "noData";
+			element["1997"] = parseInt(element["1997"], 10) || "noData";
+			element["1998"] = parseInt(element["1998"], 10) || "noData";
+			element["1999"] = parseInt(element["1999"], 10) || "noData";
+			element["2000"] = parseInt(element["2000"], 10) || "noData";
+			element["2001"] = parseInt(element["2001"], 10) || "noData";
+			element["2002"] = parseInt(element["2002"], 10) || "noData";
+			element["2003"] = parseInt(element["2003"], 10) || "noData";
+			element["2004"] = parseInt(element["2004"], 10) || "noData";
+			element["2005"] = parseInt(element["2005"], 10) || "noData";
+			element["2006"] = parseInt(element["2006"], 10) || "noData";
+			element["2007"] = parseInt(element["2007"], 10) || "noData";
+			element["2008"] = parseInt(element["2008"], 10) || "noData";
+			element["2009"] = parseInt(element["2009"], 10) || "noData";
+			element["2010"] = parseInt(element["2010"], 10) || "noData";
+			element["2011"] = parseInt(element["2011"], 10) || "noData";
+			element["2013"] = parseInt(element["2013"], 10) || "noData";
+			element["2014"] = parseInt(element["2014"], 10) || "noData";
 		});
 
 		/*  Nest the data by country code */
@@ -111,9 +122,25 @@ function buildmap () {
 				.attr("d", path)
 				.attr("fill", function (d) {
 					if ( d.values ) {
-						return color(d.values[0][year]);
+						if ( d.values[0][year] === "noData") {
+							return "#ccc";
+						} else {
+							return color(d.values[0][year]);
+						}
+
 					} else {
 						return "#ccc";
+					}
+				})
+				.on("click", function (d) {
+					if ( d.values ) {
+						if ( d.values[0][year] === "noData") {
+							console.log(d.values[0].Cname +  ": No Data");
+						} else {
+							console.log(d.values[0].Cname + ": " + d.values[0][year] + " cases");
+						}
+					} else {
+						console.log(d.id + ": No Data");
 					}
 				});
 
@@ -131,7 +158,11 @@ function buildmap () {
 				countriesSVG.selectAll("path")
 					.attr("fill", function (d) {
 						if ( d.values ) {
-							return color(d.values[0][year]);
+							if ( d.values[0][year] === "noData") {
+								return "#ccc";
+							} else {
+								return color(d.values[0][year]);
+							}
 						} else {
 							return "#ccc";
 						}
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,6 @@
 (function() {
 	var init = function($) {
 
-		// /*	Load D3 */
-		// $.getScript("http://www.nature.com/polopoly_static/js/d3.v3.min.js", function() {
-
-		// 	buildmap();
-
-		// }); /* End of d3js getscript call */
-
 		$.when( $.getScript("http://www.nature.com/polopoly_static/js/d3.v3.min.js"),
 				$.getScript("http://d3js.org/topojson.v1.min.js")
 		).then( function () {
